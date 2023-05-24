e4bcb3c0d37c27128535015eb1b1eb1d2f08123b
ChoiWonSeo <w2111@e-mirim.hs.kr>
2023-05-25 00:08:58 +0900

    form 수정 및 완성

--- js/form.js
+++ js/form.js
@@ -2,6 +2,10 @@ window.onload = function() {
     document.getElementById('form').onsubmit = 
     // 동작 시킬 function
         function() {
+            // div로 묶기
+            var container = document.createElement("div");
+            container.classList.add("container");
+
             var user = document.createElement("p"); // p 태그로 아래 글 출력하기
             user.textContent = this.name.value + "님이 사는 지역은 " + this.town.value + "입니다.";
             
@@ -9,12 +13,16 @@ window.onload = function() {
             var btn = document.createElement("button"); 
             btn.textContent = "삭제"; 
 
-            document.getElementsByTagName('body')[0].appendChild(user); // user 출력
-            document.getElementsByTagName('body')[0].appendChild(btn); // btn 출력
+            container.appendChild(user); // user를 container에 추가
+            container.appendChild(btn); // btn을 container에 추가
+            
+            // document.getElementsByTagName('body')[0].appendChild(user); // user 출력
+            // document.getElementsByTagName('body')[0].appendChild(btn); // btn 출력
+
+            document.getElementsByTagName('body')[0].appendChild(container); // container를 body에 추가
 
             btn.onclick = function() {
-                user.parentNode.removeChild(user); // user 삭제
-                btn.parentNode.removeChild(btn); // btn 삭제
+                container.parentNode.removeChild(container); //  container(user, btn) 삭제
             };
 
             // btn 누르고 출력되면 input 비우기
